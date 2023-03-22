c868cd8ba92e550446e66325fe51a425bf22f8e8
Zym0tic <lukerozon@gmail.com>
2023-03-22 15:44:12 -0400

    move code into homescreen

--- client/src/App.js
+++ client/src/App.js
@@ -13,23 +13,6 @@ function App() {
         <Routes>
           <Route path='/' element={<HomeScreen />} />
         </Routes>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {Data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-              <a href={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-                </a>
-                <p><strong>${product.price}</strong></p>
-                <button>add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
       </main>
     </div>
     </BrowserRouter>
