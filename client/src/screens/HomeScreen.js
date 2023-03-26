3818e586c4ddee204c666de8f44556ca8ab3d170
Zym0tic <lukerozon@gmail.com>
2023-03-26 13:25:43 -0400

    add loading and error handling when fetching products

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
 // import Data from '../data'
 
 const reducer = (state, action) => {
@@ -17,20 +18,20 @@ const reducer = (state, action) => {
 };
 
 function HomeScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
-    products: [], 
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
     loading: true,
     error: '',
   });
   // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      dispatch({type: 'FETCH_REQUEST'})
+      dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get('/api/products'); 
-        dispatch({type: 'FETCH_SUCCESS', payload: result.data})
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({type: 'FETCH_FAIL', payload: err.messege})
+        dispatch({ type: 'FETCH_FAIL', payload: err.messege });
       }
       // setProducts(result.data);
     };
@@ -40,22 +41,28 @@ function HomeScreen() {
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>add to cart</button>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>add to cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
