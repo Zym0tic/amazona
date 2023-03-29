49fc858f2f68d1b8998231d9cab34df7f1c63700
Zym0tic <lukerozon@gmail.com>
2023-03-28 21:12:49 -0400

    import bootstrap to indexjs and remove header and header anchor css

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,23 +8,23 @@ import LinkContainer from 'react-router-bootstrap/LinkContainer';
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <LinkContainer to="/">
-            <Navbar.Brand>amazona</Navbar.Brand>
-            </LinkContainer>
-          </Container>
-        </Navbar>
-      </header>
-      <main>
-        <Routes>
-          <Route path='/product/:slug' element={<ProductScreen />} />
-          <Route path='/' element={<HomeScreen />} />
-        </Routes>
-      </main>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
