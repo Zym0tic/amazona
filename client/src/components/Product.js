88180ad027b48ad1ef4d9c174381feef62f9a13b
Zym0tic <lukerozon@gmail.com>
2023-03-30 21:58:46 -0400

    create a responsive layout for featured products

---
+++ client/src/components/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+function Product(props) {
+  const { product } = props;
+  return (
+    <Card className="product" key={product.slug}>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>%{product.price}</Card.Text>
+          <Button>Add to cart</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Product;
