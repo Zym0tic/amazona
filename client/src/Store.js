f32f7d80e9f74b2e93cf5e79a51bd4f46200c83f
Zym0tic <lukerozon@gmail.com>
2023-04-03 23:12:55 -0400

    create store provider, and reducer function for adding items to cart

---
+++ client/src/Store.js
@@ -0,0 +1,24 @@
+import { createContext, useReducer } from 'react';
+
+export const Store = createContext();
+
+const initialState = {
+  cart: {
+    cartItems: [],
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CART_ADD_ITEM':
+    // add to cart
+    default:
+      return state;
+  }
+}
+
+export function StoreProvider(props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const vslue = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
+}
