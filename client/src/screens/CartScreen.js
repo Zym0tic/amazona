c1618f44d3887df724a8d2a28abc7cc28667645c
Zym0tic <lukerozon@gmail.com>
2023-05-16 20:17:40 -0400

    add subtotal in shopping cart

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -30,46 +30,55 @@ function CartScreen() {
             </MessageBox>
           ) : (
             <ListGroup>
-                {cartItems.map((item) => (
-                    <ListGroup.Item key={item._id}>
-                        <Row className='align-items-center'>
-                            <Col md={4}>
-                                <img 
-                                src={item.image}
-                                alt={item.name}
-                                className="img-fluid rounded img-thumbnail"
-                                ></img>{' '}
-                                <Link to={`/product/${item.slug}`}>{item.name}</Link>
-                            </Col>
-                            <Col md={3}>
-                                <Button variant="light" disabled={item.quantity === 1}>
-                                    <i className="fas fa-minus-circle"></i>
-                                </Button>{' '}
-                                <span>{item.quantity}</span>{' '}
-                                <Button variant="light" disabled={item.quantity === item.countInStock}>
-                                    <i className="fas fa-plus-circle"></i>
-                                </Button>
-                            </Col>
-                            <Col md={3}>${item.price}</Col>
-                            <Col md={2}>
-                                <Button variant="light">
-                                    <i className='fas fa-trash'></i>
-                                </Button>
-                            </Col>
-                        </Row>
-                    </ListGroup.Item>
-                ))}
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item._id}>
+                  <Row className="align-items-center">
+                    <Col md={4}>
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="img-fluid rounded img-thumbnail"
+                      ></img>{' '}
+                      <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={3}>
+                      <Button variant="light" disabled={item.quantity === 1}>
+                        <i className="fas fa-minus-circle"></i>
+                      </Button>{' '}
+                      <span>{item.quantity}</span>{' '}
+                      <Button
+                        variant="light"
+                        disabled={item.quantity === item.countInStock}
+                      >
+                        <i className="fas fa-plus-circle"></i>
+                      </Button>
+                    </Col>
+                    <Col md={3}>${item.price}</Col>
+                    <Col md={2}>
+                      <Button variant="light">
+                        <i className="fas fa-trash"></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
             </ListGroup>
           )}
         </Col>
         <Col md={4}>
-            {/* <Card>
-                <Card.Body>
-                    <ListGroup variant="flush">
-                        <ListGroupItem></ListGroupItem>
-                    </ListGroup>
-                </Card.Body>
-            </Card> */}
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h3>
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
+                    items) : $
+                    {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+                  </h3>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </div>
